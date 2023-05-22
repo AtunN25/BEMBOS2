65dceb125c24eaaa5702181ae659fe86e9fe6b0d
Frank Chaparro <franklin25.2001@gmail.com>
2023-05-22 00:48:02 -0500

    conexion de de generar boleta con la interfaz de pago

--- eventos.js
+++ eventos.js
@@ -1,5 +1,7 @@
 import { hamburguesas, menus, complementos, bebidas } from "./productos.js";
 
+var detalleDeVenta = [];
+
 var PrecioFinal =  0.0;
 var PrecioFinal_aux;
 var PrecioNeto =  0.0;
@@ -16,8 +18,10 @@ var enviar_aparecer = 0;
 //CREAR ESTOS DOS POR CADA PRODUCTO
 var texto_clasica = "";
 var clasica_cant = 1;
+
 var texto_cheese = "";
 var cheese_cant = 1;
+
 var texto_royal = "";
 var royal_cant = 1;
 
@@ -116,154 +120,285 @@ if(condition == 1 ){
   // Agregar un evento de clic al boton
   botonClasica.addEventListener("click", function() {
     // Obtener el objeto de la hamburguesa "clasica" del array
-    var hamburguesaClasica = hamburguesas.find(hamburguesa => hamburguesa.name === 'Clasica');
+  var hamburguesaClasica = hamburguesas.find(hamburguesa => hamburguesa.name === 'Clasica');
 
-    texto_clasica = "Nombre: " + "\t" + hamburguesaClasica.name + "\n" +   
-                    "Precio c/u:" + "\t" + hamburguesaClasica.precio + "\n" +
-                    "Cantidad:" + "\t" + "X" + clasica_cant + "\n";
-    
-    clasica_cant = clasica_cant + 1;
-    // Construir el texto a mostrar en el textarea
-    PrecioFinal = PrecioFinal + hamburguesaClasica.precio;
-    PrecioFinal_aux = PrecioFinal.toFixed(2);
-    PrecioNeto = PrecioFinal / 1.2;
-    IGV = PrecioFinal - PrecioNeto;
-    PrecioNeto_aux =  PrecioNeto.toFixed(2);
-    IGV_aux = IGV.toFixed(2);
+  // Verificar si el objeto ya existe en detalleDeVenta
+  var detalleClasica = detalleDeVenta.find(detalle => detalle.name === hamburguesaClasica.name);
+  if (detalleClasica) {
+    // Si ya existe, incrementar la cantidad
+    detalleClasica.cantidad += 1;
+  } else {
+    // Si no existe, agregar un nuevo objeto al arreglo
+    detalleDeVenta.push({
+      name: hamburguesaClasica.name,
+      cantidad: 1,
+      precio: hamburguesaClasica.precio
+    });
+  }
 
-    texto_precio =  "Precio Neto: " + "\t" + PrecioNeto_aux + "\n" +
-                    "IGV: " + "\t\t" + IGV_aux + "\n" +
-                    "Precio Final: " + "\t" + PrecioFinal_aux + "\n";
+  // Obtener el objeto de la hamburguesa "clasica" del array
+  var hamburguesaClasica = hamburguesas.find(hamburguesa => hamburguesa.name === 'Clasica');
 
-    texto_pedido = texto_clasica + texto_cheese + texto_royal;
-    // Asignar el texto al valor del textarea
-    textareaDescripcion1.value = texto_pedido;
-    textareaDescripcion2.value = texto_precio;
-  });
+  texto_clasica = "Nombre: " + "\t" + hamburguesaClasica.name + "\n" +   
+                  "Precio c/u:" + "\t" + hamburguesaClasica.precio + "\n" +
+                  "Cantidad:" + "\t" + "X" + clasica_cant + "\n";
+  
+  clasica_cant = clasica_cant + 1;
+  // Construir el texto a mostrar en el textarea
+  PrecioFinal = PrecioFinal + hamburguesaClasica.precio;
+  PrecioFinal_aux = PrecioFinal.toFixed(2);
+  PrecioNeto = PrecioFinal / 1.2;
+  IGV = PrecioFinal - PrecioNeto;
+  PrecioNeto_aux =  PrecioNeto.toFixed(2);
+  IGV_aux = IGV.toFixed(2);
 
-  //CHEESE
-  // Agregar un evento de clic al boton
-  botonCheese.addEventListener("click", function() {
-    // Obtener el objeto de la hamburguesa "clasica" del array
-    var hamburguesaCheese = hamburguesas.find(hamburguesa => hamburguesa.name === 'Cheese');
+  texto_precio =  "Precio Neto: " + "\t" + PrecioNeto_aux + "\n" +
+                  "IGV: " + "\t\t" + IGV_aux + "\n" +
+                  "Precio Final: " + "\t" + PrecioFinal_aux + "\n";
 
-    texto_cheese = "Nombre: " + "\t" + hamburguesaCheese.name + "\n" +   
-                    "Precio c/u:" + "\t" + hamburguesaCheese.precio + "\n" +
-                    "Cantidad:" + "\t" + "X" + cheese_cant + "\n";
+  texto_pedido = texto_clasica + texto_cheese + texto_royal;
+  // Asignar el texto al valor del textarea
+  textareaDescripcion1.value = texto_pedido;
+  textareaDescripcion2.value = texto_precio;
+});
 
-    cheese_cant = cheese_cant + 1;
-    // Construir el texto a mostrar en el textarea
-    PrecioFinal = PrecioFinal + hamburguesaCheese.precio;
-    PrecioFinal_aux = PrecioFinal.toFixed(2);
-    PrecioNeto = PrecioFinal / 1.2;
-    IGV = PrecioFinal - PrecioNeto;
-    PrecioNeto_aux =  PrecioNeto.toFixed(2);
-    IGV_aux = IGV.toFixed(2);
 
-    texto_precio =  "Precio Neto: " + "\t" + PrecioNeto_aux + "\n" +
-                    "IGV: " + "\t\t" + IGV_aux + "\n" +
-                    "Precio Final: " + "\t" + PrecioFinal_aux + "\n";
-    texto_pedido = texto_clasica + texto_cheese + texto_royal;
-    // Asignar el texto al valor del textarea
-    textareaDescripcion1.value = texto_pedido;
-    textareaDescripcion2.value = texto_precio;
-  });
 
-  //ROYAL
-  // Agregar un evento de clic al boton
-  botonRoyal.addEventListener("click", function() {
-    // Obtener el objeto de la hamburguesa "clasica" del array
-    var hamburguesaRoyal = hamburguesas.find(hamburguesa => hamburguesa.name === 'Royal');
+//CHEESE
+// Agregar un evento de clic al boton
+botonCheese.addEventListener("click", function() {
+    // Obtener el objeto de la hamburguesa "cheese" del array
+  var hamburguesaCheese = hamburguesas.find(hamburguesa => hamburguesa.name === 'Cheese');
 
-    texto_royal = "Nombre: " + "\t" + hamburguesaRoyal.name + "\n" +   
-                  "Precio c/u:" + "\t" + hamburguesaRoyal.precio + "\n" +
-                  "Cantidad:" + "\t" + "X" + royal_cant + "\n";
+  // Verificar si el objeto ya existe en detalleDeVenta
+  var detalleCheese = detalleDeVenta.find(detalle => detalle.name === hamburguesaCheese.name);
+  if (detalleCheese) {
+    // Si ya existe, incrementar la cantidad
+    detalleCheese.cantidad += 1;
+  } else {
+    // Si no existe, agregar un nuevo objeto al arreglo
+    detalleDeVenta.push({
+      name: hamburguesaCheese.name,
+      cantidad: 1,
+      precio: hamburguesaCheese.precio
+    });
+  }
+ 
+  // Obtener el objeto de la hamburguesa "clasica" del array
+  var hamburguesaCheese = hamburguesas.find(hamburguesa => hamburguesa.name === 'Cheese');
 
-    royal_cant = royal_cant + 1;
-    // Construir el texto a mostrar en el textarea
-    PrecioFinal = PrecioFinal + hamburguesaRoyal.precio;
-    PrecioFinal_aux = PrecioFinal.toFixed(2);
-    PrecioNeto = PrecioFinal / 1.2;
-    IGV = PrecioFinal - PrecioNeto;
-    PrecioNeto_aux =  PrecioNeto.toFixed(2);
-    IGV_aux = IGV.toFixed(2);
+  texto_cheese = "Nombre: " + "\t" + hamburguesaCheese.name + "\n" +   
+                  "Precio c/u:" + "\t" + hamburguesaCheese.precio + "\n" +
+                  "Cantidad:" + "\t" + "X" + cheese_cant + "\n";
 
-    texto_precio =  "Precio Neto: " + "\t" + PrecioNeto_aux + "\n" +
-                    "IGV: " + "\t\t" + IGV_aux + "\n" +
-                    "Precio Final: " + "\t" + PrecioFinal_aux + "\n";
-    texto_pedido = texto_clasica + texto_cheese + texto_royal;
-    // Asignar el texto al valor del textarea
-    textareaDescripcion1.value = texto_pedido;
-    textareaDescripcion2.value = texto_precio;
-  });
+  cheese_cant = cheese_cant + 1;
+  // Construir el texto a mostrar en el textarea
+  PrecioFinal = PrecioFinal + hamburguesaCheese.precio;
+  PrecioFinal_aux = PrecioFinal.toFixed(2);
+  PrecioNeto = PrecioFinal / 1.2;
+  IGV = PrecioFinal - PrecioNeto;
+  PrecioNeto_aux =  PrecioNeto.toFixed(2);
+  IGV_aux = IGV.toFixed(2);
 
-  botonRealizarPago.addEventListener("click", function() {
-    localStorage.setItem("pedido", texto_pedido);
-    localStorage.setItem("precio", texto_precio);
-    localStorage.setItem("precio_final", PrecioFinal_aux);
-    window.location.href = "interfazDePago.html";
-  });
+  texto_precio =  "Precio Neto: " + "\t" + PrecioNeto_aux + "\n" +
+                  "IGV: " + "\t\t" + IGV_aux + "\n" +
+                  "Precio Final: " + "\t" + PrecioFinal_aux + "\n";
+  texto_pedido = texto_clasica + texto_cheese + texto_royal;
+  // Asignar el texto al valor del textarea
+  textareaDescripcion1.value = texto_pedido;
+  textareaDescripcion2.value = texto_precio;
+
+  
+});
+
+//ROYAL
+// Agregar un evento de clic al boton
+botonRoyal.addEventListener("click", function() {
+    // Obtener el objeto de la hamburguesa "royal" del array
+  var hamburguesaRoyal = hamburguesas.find(hamburguesa => hamburguesa.name === 'Royal');
+
+  // Verificar si el objeto ya existe en detalleDeVenta
+  var detalleRoyal = detalleDeVenta.find(detalle => detalle.name === hamburguesaRoyal.name);
+  if (detalleRoyal) {
+    // Si ya existe, incrementar la cantidad
+    detalleRoyal.cantidad += 1;
+  } else {
+    // Si no existe, agregar un nuevo objeto al arreglo
+    detalleDeVenta.push({
+      name: hamburguesaRoyal.name,
+      cantidad: 1,
+      precio: hamburguesaRoyal.precio
+    });
+  }
+  // Obtener el objeto de la hamburguesa "clasica" del array
+  var hamburguesaRoyal = hamburguesas.find(hamburguesa => hamburguesa.name === 'Royal');
+
+  texto_royal = "Nombre: " + "\t" + hamburguesaRoyal.name + "\n" +   
+                "Precio c/u:" + "\t" + hamburguesaRoyal.precio + "\n" +
+                "Cantidad:" + "\t" + "X" + royal_cant + "\n";
+
+  royal_cant = royal_cant + 1;
+  // Construir el texto a mostrar en el textarea
+  PrecioFinal = PrecioFinal + hamburguesaRoyal.precio;
+  PrecioFinal_aux = PrecioFinal.toFixed(2);
+  PrecioNeto = PrecioFinal / 1.2;
+  IGV = PrecioFinal - PrecioNeto;
+  PrecioNeto_aux =  PrecioNeto.toFixed(2);
+  IGV_aux = IGV.toFixed(2);
+
+  texto_precio =  "Precio Neto: " + "\t" + PrecioNeto_aux + "\n" +
+                  "IGV: " + "\t\t" + IGV_aux + "\n" +
+                  "Precio Final: " + "\t" + PrecioFinal_aux + "\n";
+  texto_pedido = texto_clasica + texto_cheese + texto_royal;
+  // Asignar el texto al valor del textarea
+  textareaDescripcion1.value = texto_pedido;
+  textareaDescripcion2.value = texto_precio;
+  
+});
+
+
+botonRealizarPago.addEventListener("click", function() {
+  localStorage.setItem("pedido", texto_pedido);
+  localStorage.setItem("precio", texto_precio);
+  localStorage.setItem("precio_final", PrecioFinal_aux);
+  localStorage.setItem("detalleDeVenta", JSON.stringify(detalleDeVenta));
+  window.location.href = "interfazDePago.html";
+});
+
+
 }
 
 if(condition == 2){
-  texto_pedido = localStorage.getItem("pedido");
-  texto_precio = localStorage.getItem("precio");
-  PrecioFinal = localStorage.getItem("precio_final");
+texto_pedido = localStorage.getItem("pedido");
+texto_precio = localStorage.getItem("precio");
+PrecioFinal = localStorage.getItem("precio_final");
 
-  var inputPrecioFinal = document.getElementById("Precio-pago");
-  var inputDineroIngresado = document.getElementById("dinero-ingresado");
-  var inputDineroDevuelto = document.getElementById("dinero-devuelto");
+var inputPrecioFinal = document.getElementById("Precio-pago");
+var inputDineroIngresado = document.getElementById("dinero-ingresado");
+var inputDineroDevuelto = document.getElementById("dinero-devuelto");
 
-  var botonGenerarVuelto = document.getElementById("generar-vuelto");
+var botonGenerarVuelto = document.getElementById("generar-vuelto");
 
-  var textareaDescripcion_pago1 = document.querySelector('textarea[name="description-pago1"]');
-  var textareaDescripcion_pago2 = document.querySelector('textarea[name="description-pago2"]');
-  textareaDescripcion_pago1.value = texto_pedido;
-  textareaDescripcion_pago2.value = texto_precio;
-  inputPrecioFinal.value = PrecioFinal;
+var textareaDescripcion_pago1 = document.querySelector('textarea[name="description-pago1"]');
+var textareaDescripcion_pago2 = document.querySelector('textarea[name="description-pago2"]');
+textareaDescripcion_pago1.value = texto_pedido;
+textareaDescripcion_pago2.value = texto_precio;
+inputPrecioFinal.value = PrecioFinal;
 
-  botonGenerarVuelto.addEventListener("click", () => {
-    var Vuelto_aux = inputDineroIngresado.value - inputPrecioFinal.value;
-    inputDineroDevuelto.value = Vuelto_aux.toFixed(2);
+botonGenerarVuelto.addEventListener("click", () => {
+  var Vuelto_aux = inputDineroIngresado.value - inputPrecioFinal.value;
+  inputDineroDevuelto.value = Vuelto_aux.toFixed(2);
+});
+
+let factura = false;
+var GenerarBoleta = document.getElementById("Generar-boleta");
+
+var detalleDeVentaJSON = localStorage.getItem("detalleDeVenta");
+var detalleDeVenta = JSON.parse(detalleDeVentaJSON);
+console.log(detalleDeVenta)
+
+GenerarBoleta.addEventListener("click", () => {
+  const width = 150;
+  const height = 300;
+
+  const doc = new jsPDF({
+    orientation: "portrait",
+    unit: "mm",
+    format: [width, height]
   });
+
+  // Agrega el estilo de fuente al contenedor de la boleta
+  doc.setFont("Manjari", "sans-serif");
+
+  doc.setFontSize(18);
+  doc.text("BEMBOS", 60, 10);
+  doc.setFontSize(12);
+  doc.text("RUC 20392943949", 10, 20);
+  //------------------------------------------------------------
+  if(factura){
+    doc.text("FACTURA", 10, 30);
+  }else{
+    doc.text("BOLETA DE VENTA ELECTRÓNICA", 10, 30);
+  }
+  
+  //------------------------------------------------------------
+  doc.text("Jr Camino Real 18045", 10, 40);
+  const numeroBoleta = Math.floor(10000000 + Math.random() * 90000000); // Genera un número aleatorio de 8 dígitos
+  
+  doc.text("Número de boleta: " + numeroBoleta, 10, 50);
+  // Obtiene la fecha y hora actual
+  const fechaActual = new Date();
+  const fechaFormateada = fechaActual.toLocaleString();
+
+  doc.text("Fecha y hora de emisión: " + fechaFormateada, 10, 60);
+  doc.text("================================================", 10, 70);
+  doc.text("Descripción", 10, 80);
+  doc.text("Cantidad x P.unitario                   Total", 10, 90);
+  
+  let yPosition = 100;
+  let total = 0;
+
+  for (let i = 0; i < detalleDeVenta.length; i++) {
+    const producto = detalleDeVenta[i];
+    const { name, precio } = producto;
+    const cantidad = detalleDeVenta[i].cantidad;
+    const subtotal = cantidad * precio;
+
+    doc.text(name, 10, yPosition);
+    doc.text(`${cantidad} X ${precio.toFixed(2)}                                      ${subtotal.toFixed(2)}`, 10, yPosition + 10);
+
+    yPosition += 20;
+    total += subtotal;
+  }
+
+  doc.text("================================================", 10, yPosition);
+  doc.text("IGV                                                " + (total * 0.18).toFixed(2), 10, yPosition + 10);
+  doc.text("VALOR NETO                               " + (total * 0.82).toFixed(2), 10, yPosition + 20);
+  doc.text("IMPORTE TOTAL                          " + total.toFixed(2), 10, yPosition + 30);
+  doc.text("================================================", 10, yPosition + 40);
+  doc.text("Cajero: DIEGO LUCHO MAMANI QUISPE               ", 10, yPosition + 50);
+
+  doc.save("boleta.pdf");
+  localStorage.removeItem("detalleDeVenta");
+});
 
 }
 //HACER APARECER O DESAPARECER LOS BOTONES
 function aparecer_boton (desaparecer,aparecer){
-  if(desaparecer == 1){
-    btnClasica.classList.toggle("hidden");
-    btnCheese.classList.toggle("hidden");
-    btnRoyal.classList.toggle("hidden");
-  }else if(desaparecer == 2){
-    btnPechugon.classList.toggle("hidden");
-    btnBroaster.classList.toggle("hidden");
-    btnBurger.classList.toggle("hidden");
-  }else if(desaparecer == 3){
-    btnNuggets.classList.toggle("hidden");
-    btnSalchipapa.classList.toggle("hidden");
-    btnCheeseFingers.classList.toggle("hidden");
-  }else if(desaparecer == 4){
-    btnInkaKola.classList.toggle("hidden");
-    btnCocaCola.classList.toggle("hidden");
-    btnSprite.classList.toggle("hidden");
-  }
-  if(aparecer == 1){
-    btnClasica.classList.toggle("hidden");
-    btnCheese.classList.toggle("hidden");
-    btnRoyal.classList.toggle("hidden")
-  }else if(aparecer == 2){
-    btnPechugon.classList.toggle("hidden");
-    btnBroaster.classList.toggle("hidden");
-    btnBurger.classList.toggle("hidden");
-  }else if(aparecer == 3){
-    btnNuggets.classList.toggle("hidden");
-    btnSalchipapa.classList.toggle("hidden");
-    btnCheeseFingers.classList.toggle("hidden");
-  }else if(aparecer == 4){
-    btnInkaKola.classList.toggle("hidden");
-    btnCocaCola.classList.toggle("hidden");
-    btnSprite.classList.toggle("hidden");
-  }
+if(desaparecer == 1){
+  btnClasica.classList.toggle("hidden");
+  btnCheese.classList.toggle("hidden");
+  btnRoyal.classList.toggle("hidden");
+}else if(desaparecer == 2){
+  btnPechugon.classList.toggle("hidden");
+  btnBroaster.classList.toggle("hidden");
+  btnBurger.classList.toggle("hidden");
+}else if(desaparecer == 3){
+  btnNuggets.classList.toggle("hidden");
+  btnSalchipapa.classList.toggle("hidden");
+  btnCheeseFingers.classList.toggle("hidden");
+}else if(desaparecer == 4){
+  btnInkaKola.classList.toggle("hidden");
+  btnCocaCola.classList.toggle("hidden");
+  btnSprite.classList.toggle("hidden");
+}
+if(aparecer == 1){
+  btnClasica.classList.toggle("hidden");
+  btnCheese.classList.toggle("hidden");
+  btnRoyal.classList.toggle("hidden")
+}else if(aparecer == 2){
+  btnPechugon.classList.toggle("hidden");
+  btnBroaster.classList.toggle("hidden");
+  btnBurger.classList.toggle("hidden");
+}else if(aparecer == 3){
+  btnNuggets.classList.toggle("hidden");
+  btnSalchipapa.classList.toggle("hidden");
+  btnCheeseFingers.classList.toggle("hidden");
+}else if(aparecer == 4){
+  btnInkaKola.classList.toggle("hidden");
+  btnCocaCola.classList.toggle("hidden");
+  btnSprite.classList.toggle("hidden");
+}
 }
 
 
--- productos.js
+++ productos.js
@@ -1,118 +1,142 @@
 const hamburguesas = [
-    {
-      name: 'Clasica',
-      precio: 11.90
-    },
-    {
-      name: 'Cheese',
-      precio: 0.20
-    },
-    {
-      name: 'Royal',
-      precio: 0.15
-    },
-    {
-      name: 'Queso-tocino',
-      precio: 11.90
-    },
-    {
-      name: 'Broaster',
-      precio: 0.20
-    },
-    {
-      name: 'A-lo-pobre',
-      precio: 0.15
-    },
-    {
-      name: 'Carretillera',
-      precio: 11.90
-    },
-    {
-      name: 'Churrita',
-      precio: 0.20
-    },
-    {
-      name: 'Parrillera',
-      precio: 0.15
-    },
-    {
-      name: 'Extrema',
-      precio: 11.90
-    },
-    {
-      name: 'Cocona',
-      precio: 0.20
-    },
-    {
-      name: 'Pollera',
-      precio: 0.15
-    }
-];
-
-const menus = [
+  { 
+    name: 'Clasica',
+    cantidad : 0,
+    precio: 11.90
+  },
   {
-    name: 'Pechugon',
-    precio: 16.90
+    name: 'Cheese',
+    cantidad : 0,
+    precio: 0.20
+  },
+  {
+    name: 'Royal',
+    cantidad : 1,
+    precio: 0.15
+  },
+  {
+    name: 'Queso-tocino',
+    cantidad : 0,
+    precio: 11.90
   },
   {
     name: 'Broaster',
-    precio: 16.90
-  },
-  {
-    name: 'Burger',
-    precio: 16.90
-  },
-  {
-    name: 'Taypa',
-    precio: 16.90
+    cantidad : 0,
+    precio: 0.20
   },
   {
     name: 'A-lo-pobre',
-    precio: 16.90
+    cantidad : 0,
+    precio: 0.15
+  },
+  {
+    name: 'Carretillera',
+    cantidad : 0,
+    precio: 11.90
+  },
+  {
+    name: 'Churrita',
+    cantidad : 0,
+    precio: 0.20
+  },
+  {
+    name: 'Parrillera',
+    cantidad : 0,
+    precio: 0.15
+  },
+  {
+    name: 'Extrema',
+    cantidad : 0,
+    precio: 11.90
+  },
+  {
+    name: 'Cocona',
+    cantidad : 0,
+    precio: 0.20
+  },
+  {
+    name: 'Pollera',
+    cantidad : 0,
+    precio: 0.15
   }
 ];
 
+const menus = [
+{
+  name: 'Pechugon',
+  cantidad : 0,
+  precio: 16.90
+},
+{
+  name: 'Broaster',
+  cantidad : 0,
+  precio: 16.90
+},
+{
+  name: 'Burger',
+  cantidad : 0,
+  precio: 16.90
+},
+{
+  name: 'Taypa',
+  cantidad : 0,
+  precio: 16.90
+},
+{
+  name: 'A-lo-pobre',
+  cantidad : 0,
+  precio: 16.90
+}
+];
+
 const complementos = [
-  {
-    name: 'Nuggets',
-    precio: 16.90
-  },
-  {
-    name: 'Salchipapa',
-    precio: 16.90
-  },
-  {
-    name: 'Cheese-fingers',
-    precio: 16.90
-  },
-  {
-    name: 'Papas',
-    precio: 16.90
-  }
+{
+  name: 'Nuggets',
+  cantidad : 0,
+  precio: 16.90
+},
+{
+  name: 'Salchipapa',
+  cantidad : 0,
+  precio: 16.90
+},
+{
+  name: 'Cheese-fingers',
+  cantidad : 0,
+  precio: 16.90
+},
+{
+  name: 'Papas',
+  cantidad : 0,
+  precio: 16.90
+}
 ]
 
 const bebidas = [
-  {
-    name: 'Inca-kola',
-    precio: 5.90
-  },
-  {
-    name: 'Coca-cola',
-    precio: 5.90
-  },
-  {
-    name: 'Sprite',
-    precio: 5.90
-  },
-  {
-    name: 'Agua',
-    precio: 4.90
-  }
+{
+  name: 'Inca-kola',
+  cantidad : 0,
+  precio: 5.90
+},
+{
+  name: 'Coca-cola',
+  cantidad : 0,
+  precio: 5.90
+},
+{
+  name: 'Sprite',
+  cantidad : 0,
+  precio: 5.90
+},
+{
+  name: 'Agua',
+  cantidad : 0,
+  precio: 4.90
+}
 ]
 
 
 export {hamburguesas,menus,complementos,bebidas};
-
 
 
 
